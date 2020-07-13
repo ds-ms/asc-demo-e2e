62b5557c0f1ff896eb949f85cbc2ad729f9e889f
rgsubh <64013610+rgsubh@users.noreply.github.com>
2020-07-13 18:40:44 +0530

    Update updateAppConfig.sh

--- scripts/updateAppConfig.sh
+++ scripts/updateAppConfig.sh
@@ -5,3 +5,4 @@ echo ">> $2"
 echo ">> $configInput"
 curl -X PATCH -H "Authorization: Bearer $token" -H "Content-type: application/json" https://management.azure.com/$2?api-version=2019-08-01 --data "$configInput"
 
+
